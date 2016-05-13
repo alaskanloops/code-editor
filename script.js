a434387b0e508e9b5c2fb9f934f351f7676a6557
Johannes C. Meyer <alaskanloops@gmail.com>
2016-05-12 19:24:03 -0800

    add git ignore, nearly finish jquery

--- script.js
+++ script.js
@@ -2,6 +2,14 @@
  * Created by johannesmeyer on 5/11/16.
  */
 
+function updateOutput () {
+    $("iframe").contents().find("html").html(
+        "<html><head><style type='text/css'>" + $("#cssPanel").val() +
+        "</style> </head><body>" + $("#htmlPanel").val() + "</body></html>");
+
+        document.getElementById("outputPanel").contentWindow.eval($("#jsPanel").val());
+}
+
 $(".toggle-button").hover(
     function() {
         $(this).addClass("highlighted-button");
@@ -22,13 +30,9 @@ $(".toggle-button").click(function () {
 $(".panel").height($(window).height() - $("header").height() - 55 );
 $(".panel").width(($(window).width() / 2) - 10);
 
+updateOutput();
 
-$("#htmlPanel").on('change keyup paste', function () {
+$(".panel").on('change keyup paste', function () {
     $("iframe").contents().find("html").html(
-        "<html><head><style type='text/css'>" +
-        $("#cssPanel").val() +
-        "</style> </head><body>" +
-        $("#htmlPanel").val() +
-        "</body></html>");
-
+        updateOutput());
 });
